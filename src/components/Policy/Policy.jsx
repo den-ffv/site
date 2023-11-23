bfbda754eed444072e7d7992e63ed2b787c980b0
Syagr <pasha.syagro@github.com>
2023-11-23 21:26:06 +0200

    add text in translation

--- src/components/Policy/Policy.jsx
+++ src/components/Policy/Policy.jsx
@@ -1,51 +1,51 @@
+import React, { useState, useEffect } from 'react';
 import './Policy.scss';
-
 
 function Policy({ translations }) {
   return (
     <section className="policy">
       <div className="policy__container">
         <div className="policy__title">
-          <h1>{translations.policyTitle}</h1>
+          <h1>{translations.policyH1}</h1>
         </div>
         <div className="policy__text">
           <div className="policy__text-item">
-            <p className="main__information">{translations.policyMainText}</p>
+            <p className="main__information">{translations.policyTextMain}</p>
           </div>
           <div className="policy__text-point">
-            <h2>{translations.policyHeadline1}</h2>
+            <h2>{translations.policyH2_1}</h2>
             <p className="main__information">{translations.policyText1}</p>
           </div>
           <div className="policy__text-point">
-            <h2>{translations.policyHeadline2}</h2>
+            <h2>{translations.policyH2_2}</h2>
             <p className="main__information">{translations.policyText2}</p>
           </div>
           <div className="policy__text-point">
-            <h2>{translations.policyHeadline3}</h2>
+            <h2>{translations.policyH2_3}</h2>
             <p className="main__information">{translations.policyText3}</p>
           </div>
           <div className="policy__text-point">
-            <h2>{translations.policyHeadline4}</h2>
+            <h2>{translations.policyH2_4}</h2>
             <p className="main__information">{translations.policyText4}</p>
           </div>
           <div className="policy__text-point">
-            <h2>{translations.policyHeadline5}</h2>
+            <h2>{translations.policyH2_5}</h2>
             <p className="main__information">{translations.policyText5}</p>
           </div>
           <div className="policy__text-point">
-            <h2>{translations.policyHeadline6}</h2>
+            <h2>{translations.policyH2_6}</h2>
             <p className="main__information">{translations.policyText6}_1</p>
           </div>
           <div className="policy__text-point">
-            <h2>{translations.policyHeadline7}</h2>
+            <h2>{translations.policyH2_7}</h2>
             <p className="main__information">{translations.policyText7}</p>
           </div>
           <div className="policy__text-point">
-            <h2>{translations.policyHeadline8}</h2>
+            <h2>{translations.policyH2_8}</h2>
             <p className="main__information">{translations.policyText8}</p>
           </div>
           <div className="policy__text-point">
-            <h2>{translations.policyHeadline9}</h2>
+            <h2>{translations.policyH2_9}</h2>
             <p className="main__information">{translations.policyText9}</p>
           </div>
         </div>
@@ -53,5 +53,5 @@ function Policy({ translations }) {
     </section>
   );
 }
- 
+
 export default Policy;
--- src/language/englishTranslations.js
+++ src/language/englishTranslations.js
@@ -56,6 +56,58 @@ const englishTranslations = {
   leaderPartyСitation:
     '"Young people are a force that can change the world. Their energy and independence are the key to tomorrow."',
 
+  // policy section
+
+  policyH1: 'Personal Data Privacy Policy',
+  policyTextMain: 'Political Party “UDAR (Ukrainian Democratic Alliance for Reforms)
+Vitaliy Klitschko ”(hereinafter referred to as the Party) respects the privacy of personal data of users of the site www.udar.party and makes every effort to protect it from improper use.
+
+"Privacy Policy" (hereinafter referred to as policy) is aimed at public information about the order of collection (processing), storage, transfer of personal data.
+By visiting the party website, you confirm that they are familiar with and agrees with the privacy policy and norms [Law of Ukraine "On Personal Data Protection"] (https://zakon.rada.gov.ua/laws/show/2297-17#text)
+
+The following terms are used in this policy:
+Website - a collection of web pages and dependent content available on the network
+
+The Internet, which are combined both in content and in navigation under the only domain name in use of the party.
+
+The site user is a person who has gone on the website and uses it for his own purpose.
+Site User - a website user who has registered on the site by creating your account in the system in the set form.
+
+Personal data - information or a set of information about an individual who
+identified or can be specifically identified.
+
+Processing of personal data - any action or set of actions such as collection, registration, accumulation, storage, adaptation, change, renewal, use and distribution, impersonation, destruction of personal data, including using information (automated) systems.',
+policyH2_1: '1. Collection | Processing of personal data',
+    policyText1: 'The information that the website user enters at registration: surname, name, patronymic, phone number, email address, residence (registration), other information, in particular, participation in political parties, public associations, family status, place of work, desire to participate in in party development, etc.
+Data received about users of the site with the help of auxiliary web resources: date, time, period of stay on the site, visited pages, information about the device and browser, IP address, geolocation, log files, information about the device from which the site is accessed, etc. and cookies to collect personal data, in particular, the type of browser, the operating system, visiting pages on the website, the domain of the Internet provider, etc.
+Cookies are stored in a format that does not allow us to identify the user.',
+policyH2_2: '2. PURPOSE OF PERSONAL DATA PROCESSING',
+  policyText2: 'The purpose of personal data processing is the exchange of information (newsletters, event announcements, activity reports, etc.) between the Party and the User. Personal data shall be used only within the limits provided by the User and for the purposes set forth in this Policy and the Party's Charter.
+  Personal data is collected and processed in accordance with the Law of
+  of Ukraine "On Personal Data Protection". Disclosure of personal data to third parties is possible only in accordance with the procedure established by the Law.
+  The contact details provided by the User (e-mail and mobile phone number) are used for communication between the Party and the Users by means of a call to the mobile phone or by means of text messages, in particular, on the Internet (SMS, Viber, Telegram, etc.)',
+policyH2_3: '3. TERMS OF STORAGE AND DELETION OF PERSONAL DATA',
+  policyText3: 'Cookies are stored on the User's device of the Website. The User has the right to delete the relevant files at any time. Personal data provided by the
+  by a registered user is stored for the entire period of the website's operation and may be deleted under the following conditions:
+  1. At the request of the registered User, by sending a notice of the need to delete personal data from the e-mail belonging to the registered User to the Party's e-mail info@udar.party
+2. In case of violation of the terms of this Policy and the Party's Charter, in particular, in case of obscene statements, calls for the overthrow of the constitutional regime of the state, incitement to racial, interreligious, gender hatred, calls for murder and other actions aimed at violating the legislation of Ukraine by the website administrator without warning the User about it',
+    policyH2_4: '4. PROTECTION OF PERSONAL DATA',
+  policyText4: 'The party takes measures for the exclusive lawful use of personal data. The received personal data is encrypted on the website.
+  The party takes all measures to ensure the confidentiality of personal data, but warns of the possibility of adverse consequences associated with cyber attacks and other force majeure circumstances.The user is aware that confidentiality may be lost due to illegal actions of other persons',
+  policyH2_5: '5. RIGHTS OF THE WEBSITE USER',
+  policyText5: 'According to the Law of Ukraine "On Personal Data Protection", the User may contact the Party with any question regarding the use of their personal data and regarding the removal of their personal data from the website.',
+  policyH2_6: '6. ACCESS TO PERSONAL DATA',
+  policyText6: 'The party may transfer the e-mail address and mobile phone number to third parties solely for the purpose of disseminating informational messages by using telephone and electronic communication means, text messaging services (SMS, Viber, Telegram, etc.). The third party uses the data only within the framework of providing information services.
+  The user may refuse to receive such messages by clicking on the link contained in the message.',
+  policyH2_7: '7. DISCLOSURE OF PERSONAL DATA',
+  policyText7: 'The Party does not provide information about the User to third parties without the User's consent provided for by this Policy. The disclosure of personal data without the User's consent is allowed only in accordance with the procedure established by the Law of Ukraine "On Personal Data Protection"',
+  policyH2_8: '8. CHANGES TO THE PRIVACY POLICY',
+  policyText8: 'The terms of this Policy may be changed unilaterally, but all changes are not retroactive',
+  policyH2_9: '9. FINAL PROVISIONS',
+  policyText9: 'The User and the Party shall be liable for violation of the terms of the Policy in accordance with the norms of civil, administrative and criminal law.
+  All issues that are not regulated by the Policy shall be resolved in accordance with the current legislation',
+
+
   // SocialMedia section
   socialMediaTitle: 'Social media',
 
@@ -65,16 +117,12 @@ const englishTranslations = {
     'Join our team and become part of the future we are building for Ukraine together.',
 
   joinTeamTitle: 'Together we are stronger, together we will win!',
-<<<<<<< Updated upstream
-  
-=======
 
-//NotFound section
+  //NotFound section
 
   titleNotFound: 'There was some trouble. The page does not exist.',
   backToHome: 'Back home',
 
->>>>>>> Stashed changes
   // Footer
   linkItemsFooterAbout: [
     { id: 1, value: 'About us', href: '/' },
